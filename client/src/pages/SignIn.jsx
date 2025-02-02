ee1506c419250845bee1e947fed02b30d1a5f3d7
ShalukaPiuminda <piumindajayaweera@gmail.com>
2025-02-02 23:40:07 +0530

    complete signin function

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  console.log(formData);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(null);
+      setSuccess(false);
+      const res = await fetch("api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      console.log(data);
+      setLoading(false);
+      setError(null);
+      setSuccess(true);
+      setTimeout(() => {
+        navigate("/");
+      }, 1000);
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
-    <div>
-      SignIn
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-4xl text-center font-semibold my-7 space-x-2">
+        <span className="text-green-400">Sign</span>
+        <span className="text-blue-950">In</span>
+      </h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-green-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80"
+        >
+          {loading ? (
+            <ClipLoader size={20} color="#ffffff" /> // Spinner while loading
+          ) : (
+            "Sign Up"
+          )}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5 justify-center text-base">
+        <p className=""> Haven't  an Account ?</p>
+        <Link to={"/signup"}>
+          <span className="text-green-600 underline">Sign up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+      {success && <p className="text-green-500 mt-5 text-xl font-bold">Successfully signed up!</p>}
     </div>
-  )
+  );
 }
